c3821adf3157d046cc7eb9f71b4c2b975f46e7ba
somename <someemail>
2015-10-01 12:10:00 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,20 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
+	
+	SudokuVerifier sV;
+	
+	@Before
+	public void init() {
+		sV = new SudokuVerifier();
+	}
 
 	@Test
 	public void testVerifyStringLength81Characters() {
-		SudokuVerifier sV = new SudokuVerifier();
 		String characters81 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293"; 
 		assertEquals(1, sV.verify(characters81));
 	}
