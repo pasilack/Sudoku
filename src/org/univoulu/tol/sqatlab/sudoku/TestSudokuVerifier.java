08358eb61b90cd1fda61c53ccc49a9be5a9b1f59
somename <someemail>
2015-10-01 12:14:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -19,5 +19,19 @@ public class TestSudokuVerifier {
 		String characters81 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293"; 
 		assertEquals(1, sV.verify(characters81));
 	}
+	
+	@Test
+	public void testVerifyStringLength82Characters() {
+		String characters82 = "17369825632158947958724316825437169791586432346912758289643571573291684164875293"; 
+		boolean thrown = false;
+		try {
+			sV.verify(characters82);
+		}
+		catch (Exception e) {
+			thrown = true;						
+		}
+		
+		assertEquals(true, thrown);
+	}
 
 }
