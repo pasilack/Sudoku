c699d1f7c8881bcda4a856d05b0d5765134196f2
somename <someemail>
2015-10-01 13:41:10 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,8 +1,11 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
+	
+	private String cS;
 
 	public int verify(String candidateSolution) throws LengthException{
+		cS = candidateSolution;
 		
 		if (candidateSolution.length() > 81) {
 			throw new LengthException("Solution string is too long");
@@ -16,9 +19,25 @@ public class SudokuVerifier {
 				return -1;
 			}
 		}
-		
-		
+		int index = 1;
+		for(int i = 0; i<80; i++) {
+			
+			if(compare(i ,index)) {
+				return -3;
+			}
+		}
 		return 1;
+	}
+	
+	private boolean compare(int c, int index){
+		for(int i = 1; i < 9-index; i++)
+		{
+			if(cS.charAt(c) == cS.charAt(c+i))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
