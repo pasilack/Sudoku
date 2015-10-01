e11f6cee53058984ea752374420c03cc9b36c8aa
somename <someemail>
2015-10-01 12:06:32 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,8 +8,9 @@ public class TestSudokuVerifier {
 
 	@Test
 	public void testVerifyStringLength81Characters() {
+		SudokuVerifier sV = new SudokuVerifier();
 		String characters81 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293"; 
-		
+		assertEquals(1, sV.verify(characters81));
 	}
 
 }
