3566ee26fd0e1563eb95671829934d75549d5216
Pasi Lackman <lackman.pasi@gmail.com>
2015-10-08 11:21:07 +0300

    not ready
    still failing

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -22,26 +22,26 @@ public class SudokuVerifier {
 		
 		int rowIndex = 1;
 		
-//		for(int i = 0; i<80; i++) {
-//			
-//			if(i == 9 || i == 18 || i == 27 || i == 36 || i == 45 || i == 54 || i == 63 || i == 72) {
-//				rowIndex = 1;
-//			}
-//			
-//			if(compareRow(i ,rowIndex)) {
-//				return -3;
-//			}
-//			
-//			rowIndex++;
-//		}
+		for(int i = 0; i<80; i++) {
+			
+			if(i == 9 || i == 18 || i == 27 || i == 36 || i == 45 || i == 54 || i == 63 || i == 72) {
+				rowIndex = 1;
+			}
+			
+			if(compareRow(i, rowIndex)) {
+				return -3;
+			}
+			
+			rowIndex++;
+		}
 		
 		for(int i = 0; i < 9; i++) {
 			if(compareColumn(i)) {
 				return -4;
 			}
 		}
-		
-		for(int i = 0; i<76; i = i+3) {
+
+		for(int i = 0; i<60; i = i+3) {
 			if(compareSubGrid(i)) {
 				return -2;
 			}
@@ -65,7 +65,7 @@ public class SudokuVerifier {
 	}
 	
 	private boolean compareColumn(int c) {
-		for(int i = 1; i < 10; i++)
+		for(int i = 1; i < 9; i++)
 		{
 			if(cS.charAt(c) == cS.charAt(c+9*i))
 			{
@@ -76,29 +76,40 @@ public class SudokuVerifier {
 	}
 	
 	private boolean compareSubGrid(int c) {
+		String s = createSubGrid(c);
+		int rowIndex = 0;
+		
+		for(int i = 0; i<9; i++) {
+			for(int a = 1; a < 9-i; a++)
+			{
+				if(s.charAt(i) == s.charAt(i+a))
+				{
+					return true;
+				}
+			}
+			rowIndex++;
+		}
+		
+		return false;
+	}
+	
+	private String createSubGrid(int c) {
+		String subgrid = "" + cS.charAt(c);
 		for(int subgridRow = 1; subgridRow < 4; subgridRow++)
 		{
-			return true;
-//			if(subgridRow == 1) {
-//				for(int i = 1; i < 3; i++) {
-//					if(cS.charAt(c) == cS.charAt(c+i))
-//					{
-//						return true;
-//					}
-//				}
-//			}
-//			
-//			if(subgridRow == 2 || subgridRow == 3) {
-//				for(int i = 0; i < 3; i++) {
-//					if(cS.charAt(c) == cS.charAt(c+9*(subgridRow-1)+i))
-//					{
-//						return true;
-//					}
-//				}
-//			}
+			if(subgridRow == 1) {
+				for(int i = 1; i < 3; i++) {
+					subgrid += cS.charAt(c+i);
+				}
+			}
 			
+			if(subgridRow == 2 || subgridRow == 3) {
+				for(int i = 0; i < 3; i++) {
+					subgrid += cS.charAt(c+9*(subgridRow-1)+i);
+				}
+			}	
 		}
-		return true;
+		return subgrid;
 	}
 }
 
